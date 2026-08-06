60910776eeead1839a425204219a5fa1a3670cd0
AntonniSMoraes <antonnimoraes@gmail.com>
2026-08-06 19:22:34 -0300

    feat: consumo de API GraphQL

---
+++ services/api.js
@@ -0,0 +1,42 @@
+import { ENV } from '../config.js';
+
+export async function buscarAventurasDoAEM(endpointUrl) {
+    try {
+        const response = await fetch(endpointUrl, {
+            method: 'GET',
+            headers: ENV.HEADERS,
+        });
+
+        if (!response.ok) {
+            throw new Error(`Status HTTP: ${response.status}`);
+        }
+        const json = await response.json();
+        console.log('Dados recebidos do AEM:', json);
+        return json;
+
+    } catch (error) {
+        console.error('Erro na requisição ao AEM:', error);
+        return null;
+    }
+}
+
+export async function carregarImagemAutenticada(caminhoImagem) {
+    if (!caminhoImagem) return 'res/cenarios/aventurasAquaticas.png';
+
+    const urlCompleta = `${ENV.AEM_HOST}${caminhoImagem}`;
+
+    try {
+        const response = await fetch(urlCompleta, {
+            method: 'GET',
+            headers: ENV.HEADERS
+        });
+
+        if (!response.ok) throw new Error('Erro ao carregar imagem');
+
+        const blob = await response.blob();
+        return URL.createObjectURL(blob);
+    } catch (error) {
+        console.error('Erro ao carregar imagem do AEM:', error);
+        return 'res/cenarios/aventurasAquaticas.png';
+    }
+}
---
+++ services/app.js
@@ -0,0 +1,97 @@
+import { buscarAventurasDoAEM, carregarImagemAutenticada } from './api.js';
+import { ENV } from '../config.js';
+
+const cenarios = [
+    'res/cenarios/aventurasAquaticas.png',
+    'res/cenarios/escaladasCorajosas.png',
+    'res/cenarios/montanhasNevadas.png',
+    'res/cenarios/saltosRadicais.png',
+    'res/cenarios/trilhasFlorestais.png',
+];
+
+let indexAtual = 0;
+const imagemElemento = document.getElementById('imagem-cenario');
+
+function trocarImagem() {
+    if (!imagemElemento) return;
+    imagemElemento.classList.add('fade-out');
+
+    setTimeout(() => {
+        indexAtual = (indexAtual + 1) % cenarios.length;
+        imagemElemento.src = cenarios[indexAtual];
+        imagemElemento.classList.remove('fade-out');
+    }, 1000); 
+}
+
+setInterval(trocarImagem, 5000);
+
+//
+
+const dropdown = document.getElementById('filtro-dificuldade');
+const gridAventuras = document.getElementById('grid-aventuras');
+
+async function renderizarAventuras(url) {
+    if (!gridAventuras) return;
+    
+    gridAventuras.innerHTML = '<p>A carregar aventuras do AEM...</p>';
+
+    const dados = await buscarAventurasDoAEM(url);
+
+    gridAventuras.innerHTML = '';
+
+    if (dados && dados.data && dados.data.aventuraList && dados.data.aventuraList.items) {
+        const aventuras = dados.data.aventuraList.items;
+
+        if (aventuras.length === 0) {
+            gridAventuras.innerHTML = '<p>Nenhuma aventura encontrada para este filtro.</p>';
+            return;
+        }
+
+        const cardsPromessas = aventuras.map(async (aventura) => {
+            const srcImagem = await carregarImagemAutenticada(aventura.imagem?._path);
+            const srcInstrutor = await carregarImagemAutenticada(aventura.instrutor?.foto?._path);
+            return `
+                <div class="card">
+                    <img src="${srcImagem}" alt="${aventura.titulo}" class="card-imagem"/>
+                    <div style="padding: 10px;">
+                        <h3 style="margin: 0px; padding-bottom: 10px; min-height: 2.8em; line-height: 1.4; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; overflow: hidden;">
+                        ${aventura.titulo}</h3>
+                        <p style="margin: 0px;"><strong>Dificuldade:</strong> ${aventura.dificuldade}</p>
+                        <p style="margin: 0px;"><strong>Preço:</strong> $${aventura.preco}</p>
+                    </div>
+                    <div style="display: flex; align-items: center;  justify-content: space-between; padding: 10px;">
+                        <div>
+                            <p style="margin: 0px;"><strong>Instrutor:</strong> ${aventura.instrutor.nome}</p>
+                            <p style="margin: 0px;"><strong>Especialidade:</strong> ${aventura.instrutor.especialidades}</p>
+                        </div>
+                        <img src="${srcInstrutor}" alt="${aventura.instrutor.nome}" style="width: 50px; height: 50px; border-radius: 50%; object-fit: cover;"/>
+                    </div>
+                </div>
+            `;
+        });
+
+        const cardsHTML = await Promise.all(cardsPromessas);
+        gridAventuras.innerHTML = cardsHTML.join('');
+
+    } else {
+        gridAventuras.innerHTML = '<p>Erro ao processar dados recebidos do AEM.</p>';
+    }
+}
+
+if (dropdown) {
+    dropdown.addEventListener('change', function() {
+        console.log('Filtro selecionado:', dropdown.value);
+        const nivelSelecionado = dropdown.value;
+        let endpoint = '';
+
+        if (nivelSelecionado === 'todos') {
+            endpoint = `${ENV.AEM_HOST}${ENV.GRAPHQL_ENDPOINT}/allAventuras`;
+        } else {
+            endpoint = `${ENV.AEM_HOST}${ENV.GRAPHQL_ENDPOINT}/withFilter;nivel=${nivelSelecionado}`;
+        }
+
+        renderizarAventuras(endpoint);
+    });
+}
+
+renderizarAventuras(`${ENV.AEM_HOST}${ENV.GRAPHQL_ENDPOINT}/allAventuras`);
